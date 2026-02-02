f4db10d16e4c2a400bf31d10a048b097572114d8
TAEM1N2 <taemin1212@gmail.com>
2026-02-02 15:03:23 +0900

    Add PWA

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "ladder-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./사다리 타기.mp3",
+  "./장성에 대한 경례 소장.mp3"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => (key === CACHE_NAME ? null : caches.delete(key))))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const { request } = event;
+  if (request.method !== "GET") return;
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request))
+  );
+});
